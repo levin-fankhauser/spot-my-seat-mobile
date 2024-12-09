beb1b8ce3ed2f6a9a01601efbb480a0f19e3ebe9
levin-fankhauser <levin.fankhauser@gmx.ch>
2024-12-09 10:57:12 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'spot-my-seat-mobile',
+  webDir: 'www'
+};
+
+export default config;
