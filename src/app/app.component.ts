3d598e57de402d6fbc93bb9fff53a99a405c0401
levin-fankhauser <levin.fankhauser@gmx.ch>
2024-12-10 19:14:26 +0100

    Fixed bug with dark mode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -97,31 +97,24 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit() {
-    // Use matchMedia to check the user preference
     const prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
 
-    // Initialize the dark palette based on the initial
-    // value of the prefers-color-scheme media query
     this.initializeDarkPalette(prefersDark.matches);
 
-    // Listen for changes to the prefers-color-scheme media query
     prefersDark.addEventListener('change', (mediaQuery) =>
       this.initializeDarkPalette(mediaQuery.matches)
     );
   }
 
-  // Check/uncheck the toggle and update the palette based on isDark
   initializeDarkPalette(isDark: boolean) {
     this.paletteToggle = isDark;
     this.toggleDarkPalette(isDark);
   }
 
-  // Listen for the toggle check/uncheck to toggle the dark palette
   toggleChange(ev: CustomEvent) {
     this.toggleDarkPalette(ev.detail.checked);
   }
 
-  // Add or remove the "ion-palette-dark" class on the html element
   toggleDarkPalette(shouldAdd: boolean) {
     document.documentElement.classList.toggle('ion-palette-dark', shouldAdd);
   }
